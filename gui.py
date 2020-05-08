05b7984ed5cae9f09a5065d81a946532d510a259
EinarJohansson <ejrobb09@gmail.com>
2020-05-08 17:00:29 +0200

    Berättar för varnadra när de e redo

--- gui.py
+++ gui.py
@@ -4,26 +4,30 @@ import socket
 from server import Server
 import threading
 
+
 class GUI:
     def __init__(self):
         self.window = tk.Tk()
         self.window.title("Sänka skepp")
-        self.window.geometry("800x700")
+        self.window.geometry("1000x700")
         self.window.configure(bg="#142a43")
         self.host = socket.gethostbyname(socket.getfqdn())
-        self.spela()
+        self.start()
 
     def start(self):
-        self.clear() 
+        self.clear()
 
-        titel = tk.Label(self.window, text="Sänka skepp", bg="#142a43", fg="#9cffba", font=("Roboto", 70))
-        b_skapa = tk.Button(self.window, text="Skapa server", height=2, width=20, font=("Roboto", 20), command=self.skapa)
-        b_anslut = tk.Button(self.window, text="Anslut till server", height=2, width=20, font=("Roboto", 20), command=self.anslut)
+        titel = tk.Label(self.window, text="Sänka skepp",
+                         bg="#142a43", fg="#9cffba", font=("Roboto", 70))
+        b_skapa = tk.Button(self.window, text="Skapa server", height=2, width=20, font=(
+            "Roboto", 20), command=self.skapa)
+        b_anslut = tk.Button(self.window, text="Anslut till server",
+                             height=2, width=20, font=("Roboto", 20), command=self.anslut)
 
         titel.pack(pady=50)
         b_skapa.pack(pady=10)
         b_anslut.pack(pady=30)
-    
+
     def skapa(self):
         self.clear()
 
@@ -32,16 +36,21 @@ class GUI:
             tråd = threading.Thread(target=self.öra.lyssna, args=(self,))
             tråd.start()
 
-        titel = tk.Label(self.window, text="Sänka skepp", bg="#142a43", fg="#9cffba", font=("Roboto", 70))
-        info = tk.Label(self.window, text="Väntar på att någon ska ansluta till din server...", bg="#142a43", fg="grey", font=("Roboto", 20))
-        uuid = tk.Label(self.window, text=self.host, bg="#142a43", fg="white", font=("Roboto", 20))
-        
+        titel = tk.Label(self.window, text="Sänka skepp",
+                         bg="#142a43", fg="#9cffba", font=("Roboto", 70))
+        info = tk.Label(self.window, text="Väntar på att någon ska ansluta till din server...",
+                        bg="#142a43", fg="grey", font=("Roboto", 20))
+        uuid = tk.Label(self.window, text=self.host,
+                        bg="#142a43", fg="white", font=("Roboto", 20))
+
         f = font.Font(uuid, uuid.cget("font"))
         f.configure(underline=True)
         uuid.configure(font=f)
 
-        b_kopiera = tk.Button(self.window, text="Kopiera Server IP", height=2, width=20, font=("Roboto", 20), command=self.kopiera) 
-        b_avbryt = tk.Button(self.window, text="Avbryt", height=2, width=20, font=("Roboto", 20), command=self.start) 
+        b_kopiera = tk.Button(self.window, text="Kopiera Server IP",
+                              height=2, width=20, font=("Roboto", 20), command=self.kopiera)
+        b_avbryt = tk.Button(self.window, text="Avbryt", height=2, width=20, font=(
+            "Roboto", 20), command=self.start)
 
         titel.pack(pady=50)
         uuid.pack()
@@ -51,27 +60,34 @@ class GUI:
 
     def anslut(self):
         self.clear()
-        
-        titel = tk.Label(self.window, text="Sänka skepp", bg="#142a43", fg="#9cffba", font=("Roboto", 70))
+
+        titel = tk.Label(self.window, text="Sänka skepp",
+                         bg="#142a43", fg="#9cffba", font=("Roboto", 70))
         adress = tk.Entry(self.window, width=50)
-        b_anslut = tk.Button(self.window, text="Anslut", height=2, width=20, font=("Roboto", 20))
-        b_avbryt = tk.Button(self.window, text="Avbryt", height=2, width=20, font=("Roboto", 20), command=self.start) 
-       
+        b_anslut = tk.Button(self.window, text="Anslut",
+                             height=2, width=20, font=("Roboto", 20))
+        b_avbryt = tk.Button(self.window, text="Avbryt", height=2, width=20, font=(
+            "Roboto", 20), command=self.start)
+
         titel.pack(pady=50)
         adress.pack(pady=10)
         b_anslut.pack(pady=30)
         b_avbryt.pack(pady=10)
-    
+
     def spela(self):
         self.clear()
 
-        titel = tk.Label(self.window, text="Sänka skepp", bg="#142a43", fg="#9cffba", font=("Roboto", 70))
+        titel = tk.Label(self.window, text="Sänka skepp",
+                         bg="#142a43", fg="#9cffba", font=("Roboto", 70))
         p1 = tk.Canvas(self.window, width=400, height=400, bg='#142a43')
         p2 = tk.Canvas(self.window, width=400, height=400, bg='#142a43')
+        self.redo = tk.Button(self.window, text="Redo", height=2, width=20, font=(
+            "Roboto", 20), command=lambda: self.öra.redo(self))
 
         titel.pack()
-        p1.pack(side=tk.LEFT, padx=200)
-        p2.pack(side=tk.RIGHT, padx=200)
+        p1.pack(side=tk.LEFT, padx=50)
+        self.redo.pack(side=tk.LEFT, expand=tk.YES)
+        p2.pack(side=tk.RIGHT, padx=50)
 
         # Din spelplan
         for r in range(10):
@@ -84,15 +100,14 @@ class GUI:
             for c in range(10):
                 coords = (c*40, r*40, c*40+40, r*40+40)
                 p2.create_rectangle(coords, fill='#142a43', width=2)
-        
+
         # Om det är vår tur att spela så ska bara vi ha bind enablat
-        p1.bind('<Button-1>', lambda event: p1.itemconfig(tk.CURRENT, fill="blue"))
-        p2.bind('<Button-1>', lambda event: p2.itemconfig(tk.CURRENT, fill="red"))
+        p1.bind('<Button-1>', lambda event: p1.itemconfig(tk.CURRENT, fill="#9cffba"))
 
     def kopiera(self):
         self.window.clipboard_clear()
         self.window.clipboard_append(self.host)
-    
+
     def clear(self):
         # Clear window
         children = self.window.winfo_children()
--- server.py
+++ server.py
@@ -1,27 +1,67 @@
 import socket
 
+
 class Server:
     def __init__(self):
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.host = socket.gethostbyname(socket.getfqdn())
         self.port = 4444
 
-    def lyssna(self, gui):        
+    def lyssna(self, gui):
         with self.server as s:
-            s.bind((self.host, self.port))
-            s.listen()
+            try:
+                s.bind((self.host, self.port))
+                s.listen()
+            except Exception:
+                gui.start()
 
-            conn, addr = s.accept()
-            print('ansluten till', addr)
+            self.conn, self.addr = s.accept()
 
-            # Gå vidare till att starta spelet
-            gui.spela()
-
-            with conn:
-                print('Connected by', addr)
+            with self.conn:
+                print('Connected by', self.addr)
+                # Gå vidare till att starta spelet
+                gui.spela()
                 while True:
-                    data = conn.recv(1024)
+                    data = self.conn.recv(1024)
                     if not data:
                         gui.start()
+                        if hasattr(self, 'p1_starta') and hasattr(self, 'p2_starta'):
+                            self.p1_starta = False
+                            self.p2_starta = False
+                        elif hasattr(self, 'p1_starta'):
+                            self.p1_starta = False
+                        elif  hasattr(self, 'p2_starta'):
+                            self.p2_starta = False
                         break
-                    conn.sendall(data)
+                        
+                    # kolla om de är redo att starta
+                    data_str = str(data.decode('utf-8')).strip()
+
+                    print('p1 är redo?', hasattr(self, 'p1_starta'))
+
+                    if data_str == 'redo':
+                        self.p2_starta = True
+                    
+                    print('p2 är redo?', hasattr(self, 'p2_starta'))
+
+                    if hasattr(self, 'p2_starta') and hasattr(self, 'p1_starta'):
+                        print('båda är anslutna!!!')
+                        gui.redo.pack_forget()
+
+
+                    
+                    # hantera datat som kommer in
+                    # self.conn.sendall(data)
+            
+
+    def redo(self, gui):
+        try:
+            self.conn.send(b'redo')
+            self.p1_starta = True
+        except Exception:
+            gui.start()
+            return
+        
+        if hasattr(self, 'p1_starta') and hasattr(self, 'p2_starta'):
+            print('båda är anslutna!!!')
+            gui.redo.pack_forget()
